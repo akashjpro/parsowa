20b96e6cbff573e5f520114d95f1bb1ef91f7d30
Akashj Pro <akashjpro@gmail.com>
2021-10-04 23:17:19 +0700

    Refactor AppColors

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class AppColors {
-  static const primaryColor = Color(0xfff5aa56);
+  static const primaryColor = Color(0xFFF5AA56);
 
   // Nav Bottom
   static const unselectedLabelNavBottomColor = Color(0xFF2C2C2C);
