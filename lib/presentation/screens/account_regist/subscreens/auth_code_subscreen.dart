5abcfbe6e60ff5b9448f05e7110e68e59f9f37b8
linhsol26 <nhglinh.sol@gmail.com>
2021-10-18 22:54:05 +0700

    AccountInfo done

---
+++ lib/presentation/screens/account_regist/subscreens/auth_code_subscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AuthCodeSubScreen extends StatefulWidget {
+  const AuthCodeSubScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthCodeSubScreenState createState() => _AuthCodeSubScreenState();
+}
+
+class _AuthCodeSubScreenState extends State<AuthCodeSubScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.amber,
+      ),
+    );
+  }
+}
