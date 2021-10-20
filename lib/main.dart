91a1bda1b3fcbc06abc6d172b434464d1f1b3cda
Akashj Pro <akashjpro@gmail.com>
2021-10-20 20:12:46 +0700

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:parsowa/presentation/routers/router.dart';
 import 'package:parsowa/presentation/screens/login/login_screen.dart';
-import 'package:parsowa/presentation/screens/salary_detail/salary_detail.dart';
 
 import 'core/constants/strings.dart';
 import 'core/themes/app_theme.dart';
-import 'presentation/screens/account_regist/account_regist_screen.dart';
-import 'presentation/screens/account_regist/subscreens/display_camera_subscreen.dart';
-import 'presentation/screens/account_regist/subscreens/image_attach_subscreen.dart';
-import 'presentation/screens/attend_list/attend_list_screen.dart';
-import 'presentation/screens/qr_code_read/qr_code_read_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +20,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: Strings.appName,
       theme: AppTheme.lightTheme,
-      home: ImageAttachSubScreen(),
+      home: LoginScreen(title: "",),
       // home: DisplayCamera(),
       onGenerateRoute: RouteGenerator.generateRoute,
     );
